4b269a210b584ba0f7d9485de9fd88804dfb0d27
Esteban <gustavoesteban13@hotmail.com>
2021-04-07 09:21:35 -0300

    feat: implementando put delete postagem

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -1,8 +1,8 @@
+import { Postagem } from './../model/Postagem';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.prod';
-import { Postagem } from '../model/Postagem';
 
 @Injectable({
   providedIn: 'root'
@@ -19,7 +19,19 @@ export class PostagemService {
     return this.http.get<Postagem[]>('http://localhost:8080/postagens', this.token)
   }
 
+  getByIdPostagem(id: number): Observable<Postagem>{
+    return this.http.get<Postagem>(`http://localhost:8080/postagens/${id}` ,this.token )
+  }
+
   postPostagem(postagem: Postagem): Observable<Postagem>{
     return this.http.post<Postagem>('http://localhost:8080/postagens', postagem, this.token)
+  }
+
+  putPostagem(postagem: Postagem): Observable<Postagem>{
+    return this.http.put<Postagem>('http://localhost:8080/postagens', postagem, this.token)
+  }
+
+  deletePostagem(id: number){
+    return this.http.delete(`http://localhost:8080/postagens/${id}`,this.token )
   }
 }
